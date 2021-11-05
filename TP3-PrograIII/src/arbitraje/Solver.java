b58e09161a00578d395a6bc75ed01692ea3ca322
DESKTOP-4N5G45S\lucas <martinellilucas8@gmail.com>
2021-11-05 13:38:51 -0300

    No habia guardado XD

--- TP3-PrograIII/src/arbitraje/Solver.java
+++ TP3-PrograIII/src/arbitraje/Solver.java
@@ -17,19 +17,20 @@ public class Solver {
 					if (i+1 < _instancia.get_arbitros().length &&
 							partidoActual.getEquipo1().getArbitros().get(i) <= partidoActual.getEquipo1().getArbitros().get(i+1) &&
 							partidoActual.getEquipo2().getArbitros().get(i) <= partidoActual.getEquipo2().getArbitros().get(i+1)) {
+						
 						partidoActual.getEquipo1().agregarArbitro(i);
 						partidoActual.getEquipo2().agregarArbitro(i);
 						partidoActual.setArbitro(i);
 					
 					}
 					else if (i+1 < _instancia.get_arbitros().length) {
+							
 							partidoActual.getEquipo1().agregarArbitro(i+1);
 							partidoActual.getEquipo2().agregarArbitro(i+1);
 							partidoActual.setArbitro(i+1);
 						
 					}
 					i++;
-					continue;
 				}	
 				
 			}
